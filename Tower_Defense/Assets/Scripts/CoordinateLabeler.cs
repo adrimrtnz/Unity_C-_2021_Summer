a16c37dc83a87ac059ef7f09b270259beb858044
Adrián Martínez <adrimrtnz@gmail.com>
2021-08-12 21:20:13 +0200

    Enemy Waypoints Corroutine

--- Tower_Defense/Assets/Scripts/CoordinateLabeler.cs
+++ Tower_Defense/Assets/Scripts/CoordinateLabeler.cs
@@ -29,8 +29,6 @@ public class CoordinateLabeler : MonoBehaviour
         coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
         coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
 
-        Debug.Log(coordinates);
-        
         label.text = coordinates.x + "," + coordinates.y;
     }
 
---
+++ Tower_Defense/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float movementDelay = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            // Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(movementDelay);
+        }
+    }
+}
