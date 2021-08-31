b19599f3281d838739c3ee1d78b3b81e655073a9
Adrián Martínez <adrimrtnz@gmail.com>
2021-08-31 12:28:43 +0200

    Enemy Automated Spawning

---
+++ Tower_Defense/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float delayTimeInSeconds = 2f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while(true)
+        {
+            Instantiate(enemyPrefab, transform);
+            yield return new WaitForSeconds(delayTimeInSeconds);
+        }
+    }
+
+}
