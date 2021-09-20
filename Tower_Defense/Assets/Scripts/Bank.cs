bcdd67e99c71c6306cedf0456f3bde2e532a2aae
Adrián Martínez <adrimrtnz@gmail.com>
2021-09-20 11:41:15 +0200

    UI Currency Text

--- Tower_Defense/Assets/Scripts/Bank.cs
+++ Tower_Defense/Assets/Scripts/Bank.cs
@@ -2,33 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
+    [SerializeField] TextMeshProUGUI displayBalance;
     
     [SerializeField] int currentBalance; //TODO remove SerializeFiled once the game is polished
-
     public int CurrentBalance { get { return currentBalance; }}
+
 
     void Awake() 
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
