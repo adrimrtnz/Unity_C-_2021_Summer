cbe245dbdcdf7f8c8b2acaced42fa7b116f3b32a
Adrián Martínez <adrimrtnz@gmail.com>
2021-11-14 11:37:15 +0100

    Some level design

--- Tower_Defense/Assets/Scripts/CoordinateLabeler.cs
+++ Tower_Defense/Assets/Scripts/CoordinateLabeler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 
 [ExecuteAlways]
-[RequireComponent(typeof(TextMeshPro)]
+[RequireComponent(typeof(TextMeshPro))]
 public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
