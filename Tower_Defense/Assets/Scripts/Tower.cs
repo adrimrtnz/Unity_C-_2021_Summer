cb85f207514f1e9bf9e213c9c6a72222ec31017e
Adrián Martínez <adrimrtnz@gmail.com>
2022-06-21 16:25:02 +0200

    Building timer

--- Tower_Defense/Assets/Scripts/Tower.cs
+++ Tower_Defense/Assets/Scripts/Tower.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1.0f;
 
-    Bank bank;
+    void Start() 
+    {
+        StartCoroutine(Build());    
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
-        bank = FindObjectOfType<Bank>();
+        Bank bank = FindObjectOfType<Bank>();
         if (bank == null) { return false; }
 
         if(bank.CurrentBalance >= cost) 
@@ -21,5 +25,32 @@ public class Tower : MonoBehaviour
         }
 
         return false; 
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+
+            foreach(Transform grandchild in child)
+            {
+                    grandchild.gameObject.SetActive(false);
+            }  
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(buildDelay);
+
+            foreach(Transform grandchild in child)
+            {
+                    grandchild.gameObject.SetActive(true);
+            } 
+        }
+
+
     }
 }
